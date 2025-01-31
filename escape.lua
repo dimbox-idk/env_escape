7b723fb1ad9f8774aea89f8e05ecddae53aeb3b2
dimbox-idk <2022nsvetlana@gmail.com>
2025-02-01 00:11:31 +0200

    Update escape.lua
    pls work i put all my brain in that code

--- escape.lua
+++ escape.lua
@@ -1,103 +1,188 @@
-if not getgenv then return "UnSupported", "getgenv = nil" end
-if type(getgenv) ~= 'function' then return "UnSupported", "getgenv != function" end
-if not loadstring then return "UnSupported", "loadstring = nil" end
-if type(loadstring) ~= 'function' then return "UnSupported", "loadstring != function" end
-
-local cache = {}
-cache.getgenv = getgenv
-getgenv().getgenv = nil
-getgenv = nil
-getgenv = function() return { game = game } end
-local function s(i, v)
-	getfenv(2)[i] = v
-    getfenv(1)[i] = v
-	getfenv(0)[i] = v
-	getfenv(debug.info(0, 'f'))[i] = v
-	getfenv(debug.info(1, 'f'))[i] = v
-	getfenv(debug.info(2, 'f'))[i] = v
+if not getgenv then
+    return "UnSupported", "getgenv = nil";
 end
-s("getgenv", nil)
-s("getgenv", function() return { game = game } end)
 
-getfenv(loadstring).getgenv = getgenv
+if (type(getgenv) ~= "function") then
+    return "UnSupported", "getgenv != function";
+end
+
+if not loadstring then
+    return "UnSupported", "loadstring = nil";
+end
+
+if (type(loadstring) ~= "function") then
+    return "UnSupported", "loadstring != function";
+end
+
+local cache = {};
+cache.getgenv = getgenv;
+getgenv().getgenv = nil;
+getgenv = nil;
+local FAKE_ENV = {};
+
+function getgenv()
+    return FAKE_ENV;
+end
+
+FAKE_ENV = { game = game, loadstring = loadstring, getgenv = getgenv };
+
+local function s(i, v)
+    getfenv(2)[i] = v;
+    getfenv(1)[i] = v;
+    getfenv(0)[i] = v;
+    getfenv()[i] = v;
+    getfenv(debug.info(0, "f"))[i] = v;
+    getfenv(debug.info(1, "f"))[i] = v;
+    getfenv(debug.info(2, "f"))[i] = v;
+end
+
+s("getgenv", getgenv);
+
+setmetatable(getgenv(), {
+    __newindex = function(arg1, arg2, arg3)
+        rawset(arg1, arg2, arg3);
+        s(arg2, arg3);
+    end;
+});
+
+getfenv(loadstring).getgenv = getgenv;
 
 local BYPASSED_ENV = loadstring([===[
     if getgenv then getgenv().getgenv = nil end
-	getgenv = nil
-	getgenv = function() return { game = game } end
+    getgenv = nil
+    local FAKE_ENV = {}
+    getgenv = function() return FAKE_ENV end
+    FAKE_ENV = { game = game, loadstring = loadstring, getgenv = getgenv }
+    
     local function s(i, v)
-		getfenv(2)[i] = v
+        getfenv(2)[i] = v
         getfenv(1)[i] = v
-	    getfenv(0)[i] = v
-	    getfenv(debug.info(0, 'f'))[i] = v
-	    getfenv(debug.info(1, 'f'))[i] = v
-	    getfenv(debug.info(2, 'f'))[i] = v
+        getfenv(0)[i] = v
+        getfenv()[i] = v
+        getfenv(debug.info(0, 'f'))[i] = v
+        getfenv(debug.info(1, 'f'))[i] = v
+        getfenv(debug.info(2, 'f'))[i] = v
     end
-	s("getgenv", nil)
-	s("getgenv", function() return { game = game } end)
-    return loadstring([==[
-	    if getgenv then getgenv().getgenv = nil end
-		getgenv = nil
-	    getgenv = function() return { game = game } end
+
+    s("getgenv", getgenv)
+
+    setmetatable(getgenv(), {
+        __newindex = function(arg1, arg2, arg3)
+            rawset(arg1, arg2, arg3)
+            s(arg2, arg3)
+        end;
+    })
+
+    getfenv(loadstring).getgenv = getgenv
+
+    return loadstring([===[
+        if getgenv then getgenv().getgenv = nil end
+        getgenv = nil
+        local FAKE_ENV = {}
+        getgenv = function() return FAKE_ENV end
+        FAKE_ENV = { game = game, loadstring = loadstring, getgenv = getgenv }
+        
         local function s(i, v)
-		    getfenv(2)[i] = v
+            getfenv(2)[i] = v
             getfenv(1)[i] = v
-	        getfenv(0)[i] = v
-	        getfenv(debug.info(0, 'f'))[i] = v
-	        getfenv(debug.info(1, 'f'))[i] = v
-	        getfenv(debug.info(2, 'f'))[i] = v
+            getfenv(0)[i] = v
+            getfenv()[i] = v
+            getfenv(debug.info(0, 'f'))[i] = v
+            getfenv(debug.info(1, 'f'))[i] = v
+            getfenv(debug.info(2, 'f'))[i] = v
         end
-	    s("getgenv", nil)
-	    s("getgenv", function() return { game = game } end)
+
+        s("getgenv", getgenv)
+
+        setmetatable(getgenv(), {
+            __newindex = function(arg1, arg2, arg3)
+                rawset(arg1, arg2, arg3)
+                s(arg2, arg3)
+            end;
+        })
+
+        getfenv(loadstring).getgenv = getgenv
+
         return loadstring([=[
-	        if getgenv then getgenv().getgenv = nil end
-			getgenv = nil
-	        getgenv = function() return { game = game } end
+            if getgenv then getgenv().getgenv = nil end
+            getgenv = nil
+            local FAKE_ENV = {}
+            getgenv = function() return FAKE_ENV end
+            FAKE_ENV = { game = game, loadstring = loadstring, getgenv = getgenv }
+
             local function s(i, v)
-		        getfenv(2)[i] = v
+                getfenv(2)[i] = v
                 getfenv(1)[i] = v
-	            getfenv(0)[i] = v
-	            getfenv(debug.info(0, 'f'))[i] = v
-	            getfenv(debug.info(1, 'f'))[i] = v
-	            getfenv(debug.info(2, 'f'))[i] = v
+                getfenv(0)[i] = v
+                getfenv()[i] = v
+                getfenv(debug.info(0, 'f'))[i] = v
+                getfenv(debug.info(1, 'f'))[i] = v
+                getfenv(debug.info(2, 'f'))[i] = v
             end
-	        s("getgenv", nil)
-	        s("getgenv", function() return { game = game } end)
+
+            s("getgenv", getgenv)
+
+            setmetatable(getgenv(), {
+                __newindex = function(arg1, arg2, arg3)
+                    rawset(arg1, arg2, arg3)
+                    s(arg2, arg3)
+                end;
+            })
+
+            getfenv(loadstring).getgenv = getgenv
+
             return loadstring([[
-	            if getgenv then getgenv().getgenv = nil end
-				getgenv = nil
-	            getgenv = function() return { game = game } end
+                if getgenv then getgenv().getgenv = nil end
+                getgenv = nil
+                local FAKE_ENV = {}
+                getgenv = function() return FAKE_ENV end
+                FAKE_ENV = { game = game, loadstring = loadstring, getgenv = getgenv }
+
                 local function s(i, v)
-	                getfenv(2)[i] = v
+                    getfenv(2)[i] = v
                     getfenv(1)[i] = v
-	                getfenv(0)[i] = v
-	                getfenv(debug.info(0, 'f'))[i] = v
-	                getfenv(debug.info(1, 'f'))[i] = v
-	                getfenv(debug.info(2, 'f'))[i] = v
+                    getfenv(0)[i] = v
+                    getfenv()[i] = v
+                    getfenv(debug.info(0, 'f'))[i] = v
+                    getfenv(debug.info(1, 'f'))[i] = v
+                    getfenv(debug.info(2, 'f'))[i] = v
                 end
-	            s("getgenv", nil)
-	            s("getgenv", function() return { game = game } end)
-                local res, why = pcall(function() -- help me to replace that check please
+
+                s("getgenv", getgenv)
+
+                setmetatable(getgenv(), {
+                    __newindex = function(arg1, arg2, arg3)
+                        rawset(arg1, arg2, arg3)
+                        s(arg2, arg3)
+                    end;
+                })
+
+                getfenv(loadstring).getgenv = getgenv
+
+                local res, why = pcall(function()
                     local S = Game:GetService("ScriptContext")
-
                     local file = "BYPASSED_ENV_TEST.EXE"
-
                     local textToSave = "TEST"
                     local filereal = S:SaveScriptProfilingData(textToSave, file)
                 end)
+
                 if res then
                     local env = getfenv(0)
                     env.game = Game
+                    env.getgenv().getgenv = nil
+                    env.getgenv = nil
+                    env.loadstring = nil
                     return env
                 else
-	                return why
+                    return why
                 end
             ]])()
         ]=])()
     ]==])()
-]===])()
-getgenv = cache.getgenv
-getfenv(loadstring).getgenv = cache.getgenv
-getfenv(debug.info(0, 'f')).getgenv = cache.getgenv
-getfenv(debug.info(1, 'f')).getgenv = cache.getgenv
-return BYPASSED_ENV
+]===])();
+
+getgenv = cache.getgenv;
+getfenv(loadstring).getgenv = cache.getgenv;
+s("getgenv", cache.getgenv)
+
+return BYPASSED_ENV;
